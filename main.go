dc43e7107464190fb9cce89a291f3986cdb6d809
YANA <30638175+WhereIsX@users.noreply.github.com>
2021-02-26 18:13:54 -0500

    new feature: "Q" also quits
    hair other stuffs

--- main.go
+++ main.go
@@ -1,63 +1,71 @@
-package main 
+package main
 
 import (
 	"fmt"
-	"os"
 	tea "github.com/charmbracelet/bubbletea"
-	"github.com/muesli/termenv" 
+	"github.com/muesli/termenv"
+	"os"
 )
 
 var term = termenv.ColorProfile()
 
 func main() {
 	initCat := conciergeCat{
-		services: [3]string{"create", "get", "delete"},
-		currentService: -1,
+		clientID:          "test",
+		authToken:         "test",
+		broadcasterUserID: "more tests",
+		// services: [3]string{"create", "get", "delete"},
+		view: "signup",
 	}
 
 	p := tea.NewProgram(initCat)
 	if err := p.Start(); err != nil {
 		fmt.Printf("You WAT: %v", err)
 		os.Exit(1)
-	}	
+	}
 }
 
 // To play nice with BubbleTea, we need:
-// a model -- guestServices
+// a model -- conciergeCat
 // Init(), Update(), and View()
 
-// the model 
-type conciergeCat struct{
-	services [3]string 
-	currentService int // this points to which service we want 
+// the model
+type conciergeCat struct {
+	// services [3]string
+	// currentService int // this points to which service we want
+	view               string // options: "main", "create", "read", "delete", "delete_all"
+	clientID           string
+	authToken          string
+	broadcasterUserID  string
+	userFinishedSignup bool
 }
-
 
 // BubbleTea: Init
 func (cc conciergeCat) Init() tea.Cmd {
 	return nil
 }
 
-// BubbleTea: Update 
+// BubbleTea: Update
 func (cc conciergeCat) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
-	case tea.KeyMsg: 	// keyboard input?!
+	case tea.KeyMsg: // keyboard input?!
 		switch msg.String() {
-		case "ctrl+c", "q":
+		case "ctrl+c", "q", "Q":
 			return cc, tea.Quit
 		case "up", "k":
-			
+
 		case "down", "j":
-			
+
 		case "enter", " ":
-			
+
 		}
 	}
 	return cc, nil
 }
 
-// BubbleTea: View 
+// BubbleTea: View
 func (cc conciergeCat) View() string {
+
 	display := "test test"
 	return display + cc.viewFooter()
 }
